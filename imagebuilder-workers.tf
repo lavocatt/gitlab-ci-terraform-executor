86187957e6cca86d2b7c5f18e40c0640c7867ddf
Major Hayden <major@redhat.com>
2020-11-16 08:08:44 -0600

    Add policy for vmimport role
    Signed-off-by: Major Hayden <major@redhat.com>

--- imagebuilder-workers.tf
+++ imagebuilder-workers.tf
@@ -1,4 +1,86 @@
 ##############################################################################
+## AWS VM IMPORT REQUIREMENTS
+# NOTE(mhayden): Much of this policy comes from the AWS docs for vmimport:
+# https://docs.aws.amazon.com/vm-import/latest/userguide/vmie_prereqs.html
+
+# Create vmimport trust policy.
+data "aws_iam_policy_document" "vmimport_trust" {
+  statement {
+    sid = "1"
+
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["vmie.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "sts:Externalid"
+      values   = ["vmimport"]
+    }
+
+  }
+}
+
+# Create vmimport base policy. This allows EC2 to get images from our S3
+# buckets and take actions on snapshots/AMIs during import.
+data "aws_iam_policy_document" "vmimport_base" {
+  statement {
+    sid = "1"
+
+    actions = [
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:ListBucket"
+    ]
+
+    resources = [
+      "arn:aws:s3:::imagebuilder-stage",
+      "arn:aws:s3:::imagebuilder-stage/*",
+      "arn:aws:s3:::imagebuilder-prod",
+      "arn:aws:s3:::imagebuilder-prod/*"
+    ]
+  }
+
+  statement {
+    sid = "2"
+
+    actions = [
+      "ec2:ModifySnapshotAttribute",
+      "ec2:CopySnapshot",
+      "ec2:RegisterImage",
+      "ec2:Describe*"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+# Load the vmimport base policy.
+resource "aws_iam_policy" "vmimport_base" {
+  name   = "vmimport_base"
+  policy = data.aws_iam_policy_document.vmimport_base.json
+}
+
+# Create the vmimport role.
+resource "aws_iam_role" "vmimport" {
+  name               = "vmimport"
+  assume_role_policy = data.aws_iam_policy_document.vmimport_trust.json
+
+  tags = merge(
+    var.imagebuilder_tags, { Name = "Image Builder vmimport role" },
+  )
+}
+
+# Attach base vmimport policy to role.
+resource "aws_iam_role_policy_attachment" "vmimport_base" {
+  role       = aws_iam_role.vmimport.name
+  policy_arn = aws_iam_policy.vmimport_base.arn
+}
+
+##############################################################################
 ## STAGE
 # Create S3 bucket for stage workers to upload images.
 resource "aws_s3_bucket" "imagebuilder_stage" {
