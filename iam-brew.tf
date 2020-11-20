b3197f73fffce95f36dfdeb207e608e93afb5762
Major Hayden <major@redhat.com>
2020-11-20 13:14:42 -0600

    Add instance profile for brew role
    Signed-off-by: Major Hayden <major@redhat.com>

--- iam-brew.tf
+++ iam-brew.tf
@@ -47,6 +47,12 @@ resource "aws_iam_role" "brew_infrastructure" {
   )
 }
 
+# Link an instance profile to the role.
+resource "aws_iam_instance_profile" "brew_infrastructure" {
+  name = "brew_infrastructure"
+  role = aws_iam_role.brew_infrastructure.name
+}
+
 # Attach the Brew secrets policy to the Brew role.
 resource "aws_iam_role_policy_attachment" "brew_read_keys" {
   role       = aws_iam_role.brew_infrastructure.name
