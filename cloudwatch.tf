2a913f12de1d014f73069443edaeacc74c672ddb
Major Hayden <major@redhat.com>
2020-12-03 14:29:24 -0600

    Add workspace name to cloudwatch log group
    Signed-off-by: Major Hayden <major@redhat.com>

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -2,10 +2,10 @@
 ## INTERNAL COMPOSER SYSLOG
 # Create a log group that can contain multiple streams.
 resource "aws_cloudwatch_log_group" "internal_composer" {
-  name = "internal_composer"
+  name = "internal_composer_${local.workspace_name}"
 
   tags = merge(
-    var.imagebuilder_tags, { Name = "Internal composer log group" },
+    var.imagebuilder_tags, { Name = "Internal composer log group for ${local.workspace_name}" },
   )
 }
 
