f93495b19d72ac3408440653228f35c29ae600b0
Major Hayden <major@redhat.com>
2020-12-04 14:36:44 -0600

    Add comment for workspace_name variable
    Signed-off-by: Major Hayden <major@redhat.com>

--- locals.tf
+++ locals.tf
@@ -1,6 +1,7 @@
 locals {
+  # Set the workspace_name without the imagebuilder- prefix.
   workspace_name = var.TFC_WORKSPACE_NAME != "" ? trimprefix(var.TFC_WORKSPACE_NAME, "imagebuilder-") : terraform.workspace
 
   # Ensure that staging/stable composer get the same IP address every time.
-  network_interface_ip_address_index = local.TFC_WORKSPACE_NAME == "staging" ? 11 : 10
+  network_interface_ip_address_index = local.workspace_name == "staging" ? 11 : 10
 }
