a0a930eedbd3f2d424c7e2ef8217bd6f624110d8
Major Hayden <major@redhat.com>
2020-12-10 09:38:57 -0600

    Update to new packer image version
    Signed-off-by: Major Hayden <major@redhat.com>

--- cloud-init/worker/worker-setup.sh
+++ cloud-init/worker/worker-setup.sh
@@ -1,24 +1,9 @@
-# Forward systemd journal to the console for easier viewing.
-# This is done at the beginning of this script so we log all errors.
-mkdir -p /etc/systemd/journald.conf.d/
-tee /etc/systemd/journald.conf.d/forward-to-console.conf > /dev/null << EOF
-[Journal]
-ForwardToConsole=yes
-MaxLevelConsole=6
-EOF
-
-# Ensure the SELinux contexts are correct.
-restorecon -Rv /etc/systemd
-
-# Restart journald to pick up the console log change.
-systemctl restart systemd-journald
-
 # Variables for the script.
 COMPOSER_DIR=/etc/osbuild-composer
 
 # Set up /etc/hosts
 # TODO(mhayden): We need to convert this to DNS later when we launch.
-cat <<< "${COMPOSER_ADDRESS} ${COMPOESR_HOST}" >> /etc/hosts
+cat <<< "${COMPOSER_ADDRESS} ${COMPOSER_HOST}" >> /etc/hosts
 
 # Deploy the composer CA certificate.
 mkdir ${COMPOSER_DIR}
@@ -32,4 +17,4 @@ jq -r ".worker_crt" /tmp/worker_keys.json | base64 -d - > ${COMPOSER_DIR}/worker
 rm -f /tmp/worker_keys.json
 
 # Prepare osbuild-composer's remote worker services and sockets.
-systemctl enable --now osbuild-remote-worker@${COMPOESR_HOST}:8700
+systemctl enable --now osbuild-remote-worker@${COMPOSER_HOST}:8700
