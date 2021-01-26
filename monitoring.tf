4671a7bb05d7475b178e18965bb3eca13da9a5ff
Major Hayden <major@redhat.com>
2021-01-26 16:28:16 -0600

    Install upstream pip
    Signed-off-by: Major Hayden <major@redhat.com>

--- monitoring.tf
+++ monitoring.tf
@@ -52,7 +52,7 @@ resource "null_resource" "pip" {
   }
 
   provisioner "local-exec" {
-    command = "pip3 install -r ${path.root}/lambda/requirements.txt -t lambda/lib"
+    command = "pip install -r ${path.root}/lambda/requirements.txt -t lambda/lib"
   }
 }
 
