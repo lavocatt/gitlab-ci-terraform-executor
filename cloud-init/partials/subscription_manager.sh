a244698896cfb7a66b96eb4fcba0bd6f39dffb37
Ondřej Budai <ondrej@budai.cz>
2021-01-28 11:35:18 +0100

    Don't rely on the default secretsmanager API endpoint
    For subscribing the internal composer/workers, we can't access the default
    endpoint.

--- cloud-init/partials/subscription_manager.sh
+++ cloud-init/partials/subscription_manager.sh
@@ -6,6 +6,7 @@ echo "Subscribing instance to RHN."
 
 # Register the instance with RHN.
 /usr/local/bin/aws secretsmanager get-secret-value \
+  --endpoint-url ${SECRETS_MANAGER_ENDPOINT_URL} \
   --secret-id ${SUBSCRIPTION_MANAGER_COMMAND_ARN} | jq -r ".SecretString" > /tmp/subscription_manager_command.json
 jq -r ".subscription_manager_command" /tmp/subscription_manager_command.json | bash
 rm -f /tmp/subscription_manager_command.json
--- cloud-init/partials/worker_keys.sh
+++ cloud-init/partials/worker_keys.sh
@@ -9,6 +9,7 @@ base64 -d - <<< ${OSBUILD_CA_CERT} > ${COMPOSER_DIR}/ca-crt.pem
 
 # Deploy the composer key and certificate.
 /usr/local/bin/aws secretsmanager get-secret-value \
+  --endpoint-url ${SECRETS_MANAGER_ENDPOINT_URL} \
   --secret-id ${WORKER_SSL_KEYS_ARN} | jq -r ".SecretString" > /tmp/worker_keys.json
 jq -r ".worker_key" /tmp/worker_keys.json | base64 -d - > ${COMPOSER_DIR}/worker-key.pem
 jq -r ".worker_crt" /tmp/worker_keys.json | base64 -d - > ${COMPOSER_DIR}/worker-crt.pem
