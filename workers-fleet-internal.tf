731612654d6e96abebcd580b0a605bdb04de64e7
Ondřej Budai <ondrej@budai.cz>
2021-02-01 14:14:47 +0100

    add forgotten cloudwatch_logs_endpoint_domain to workers
    :facepalm:

--- workers-fleet-internal.tf
+++ workers-fleet-internal.tf
@@ -24,6 +24,7 @@ data "template_file" "internal_workers_cloud_config" {
 
     # TODO: pick dns name from the right availability zone
     secrets_manager_endpoint_domain = aws_vpc_endpoint.internal_vpc_secretsmanager.dns_entry[0]["dns_name"]
+    cloudwatch_logs_endpoint_domain = aws_vpc_endpoint.internal_vpc_cloudwatch_logs.dns_entry[0]["dns_name"]
 
     # Set the hostname of the instance.
     system_hostname_prefix = "${local.workspace_name}-internal-worker"
