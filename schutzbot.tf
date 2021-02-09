324cd296ff708022adc016c888753e473acd0164
Major Hayden <major@redhat.com>
2021-02-09 12:11:42 -0600

    Add API deployment
    Signed-off-by: Major Hayden <major@redhat.com>

--- schutzbot.tf
+++ schutzbot.tf
@@ -194,7 +194,7 @@ resource "aws_api_gateway_rest_api" "schutzbot_receiver_api" {
 }
 
 resource "aws_api_gateway_resource" "schutzbot_receiver_resource" {
-  path_part   = "github-webhook"
+  path_part   = "{proxy+}"
   parent_id   = aws_api_gateway_rest_api.schutzbot_receiver_api.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.schutzbot_receiver_api.id
 }
@@ -202,17 +202,27 @@ resource "aws_api_gateway_resource" "schutzbot_receiver_resource" {
 resource "aws_api_gateway_method" "schutzbot_receiver_method" {
   rest_api_id   = aws_api_gateway_rest_api.schutzbot_receiver_api.id
   resource_id   = aws_api_gateway_resource.schutzbot_receiver_resource.id
-  http_method   = "POST"
+  http_method   = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "schutzbot_receiver_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.schutzbot_receiver_api.id
-  resource_id             = aws_api_gateway_resource.schutzbot_receiver_resource.id
-  http_method             = aws_api_gateway_method.schutzbot_receiver_method.http_method
+  rest_api_id = aws_api_gateway_rest_api.schutzbot_receiver_api.id
+  resource_id = aws_api_gateway_resource.schutzbot_receiver_resource.id
+  http_method = aws_api_gateway_method.schutzbot_receiver_method.http_method
+  uri         = aws_lambda_function.schutzbot_receiver_lambda.invoke_arn
+
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.schutzbot_receiver_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "schutzbot_receiver_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.schutzbot_receiver_integration
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.schutzbot_receiver_api.id
+  stage_name  = "test"
 }
 
 resource "aws_lambda_permission" "schutzbot_receiver_permission" {
@@ -220,9 +230,7 @@ resource "aws_lambda_permission" "schutzbot_receiver_permission" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.schutzbot_receiver_lambda.function_name
   principal     = "apigateway.amazonaws.com"
-
-  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.schutzbot_receiver_api.id}/*/${aws_api_gateway_method.schutzbot_receiver_method.http_method}${aws_api_gateway_resource.schutzbot_receiver_resource.path}"
+  source_arn    = "${aws_api_gateway_rest_api.schutzbot_receiver_api.execution_arn}/*/*"
 }
 
 ##############################################################################
