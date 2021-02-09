ea61a90f78aaf8c36bba5b3fe0d5e37d1b8ffc6a
Major Hayden <major@redhat.com>
2021-02-09 10:14:18 -0600

    Fix API gateway resource path
    Signed-off-by: Major Hayden <major@redhat.com>

--- schutzbot.tf
+++ schutzbot.tf
@@ -194,7 +194,7 @@ resource "aws_api_gateway_rest_api" "schutzbot_receiver_api" {
 }
 
 resource "aws_api_gateway_resource" "schutzbot_receiver_resource" {
-  path_part   = "resource"
+  path_part   = "github-webhook"
   parent_id   = aws_api_gateway_rest_api.schutzbot_receiver_api.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.schutzbot_receiver_api.id
 }
