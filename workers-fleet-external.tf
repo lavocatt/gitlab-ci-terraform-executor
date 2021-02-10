9258635563c62fd6a4259d86e6c3ee5571c93af5
Major Hayden <major@redhat.com>
2021-02-10 09:55:20 -0600

    👷 Dynamic worker counts
    Add a local variable for a count of workers for different deployments.
    Set staging to 1 and stable to 2.
    
    Signed-off-by: Major Hayden <major@redhat.com>

--- workers-fleet-external.tf
+++ workers-fleet-external.tf
@@ -138,7 +138,7 @@ resource "aws_spot_fleet_request" "workers_external_x86" {
 
   # Keep the fleet at the target_capacity at all times.
   fleet_type      = "maintain"
-  target_capacity = 1
+  target_capacity = local.spot_fleet_worker_count
 
   # IAM role that the spot fleet service can use.
   iam_fleet_role = aws_iam_role.spot_fleet_tagging_role.arn
