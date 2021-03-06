50a3e24d2e6e74b23a09e78f29a0720310e363d0
Tom Gundersen <teg@jklm.no>
2021-03-06 17:42:06 +0000

    worker/creds: fix typo

--- cloud-init/partials/worker_external_gcp_creds.sh
+++ cloud-init/partials/worker_external_gcp_creds.sh
@@ -12,7 +12,7 @@ echo "Deploy cloud credentials for workers."
 # Deploy the Azure credentials file.
 /usr/local/bin/aws secretsmanager get-secret-value \
   --endpoint-url ${SECRETS_MANAGER_ENDPOINT_URL} \
-  --secret-id ${GCP_SERVICE_ACCOUNT_IMAGE_BUILDER_ARN} | jq -r ".SecretString" > /tmp/azure_credentials.json
+  --secret-id ${AZURE_ACCOUNT_IMAGE_BUILDER_ARN} | jq -r ".SecretString" > /tmp/azure_credentials.json
 CLIENT_ID=$(jq -r ".client_id" /tmp/azure_credentials)
 CLIENT_SECRET=$(jq -r ".client_secret" /tmp/azure_credentials)
 rm /tmp/azure_credentials.json
