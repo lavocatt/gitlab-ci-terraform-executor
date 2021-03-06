75ccf2db5dc91448187f989024df802044b40d6f
Tom Gundersen <teg@jklm.no>
2021-03-06 17:57:05 +0000

    worker/creds: fix another typo
    JSON suffix was missing.

--- cloud-init/partials/worker_external_gcp_creds.sh
+++ cloud-init/partials/worker_external_gcp_creds.sh
@@ -13,8 +13,8 @@ echo "Deploy cloud credentials for workers."
 /usr/local/bin/aws secretsmanager get-secret-value \
   --endpoint-url ${SECRETS_MANAGER_ENDPOINT_URL} \
   --secret-id ${AZURE_ACCOUNT_IMAGE_BUILDER_ARN} | jq -r ".SecretString" > /tmp/azure_credentials.json
-CLIENT_ID=$(jq -r ".client_id" /tmp/azure_credentials)
-CLIENT_SECRET=$(jq -r ".client_secret" /tmp/azure_credentials)
+CLIENT_ID=$(jq -r ".client_id" /tmp/azure_credentials.json)
+CLIENT_SECRET=$(jq -r ".client_secret" /tmp/azure_credentials.json)
 rm /tmp/azure_credentials.json
 
 sudo tee /etc/osbuild-worker/azure_credentials.toml > /dev/null << EOF
