356d20c6861c2e08683435082cca99b816c3d629
David Rheinsberg <david.rheinsberg@gmail.com>
2021-03-08 16:27:09 +0100

    rpmrepo: implement API Gateway REST API
    Add the API-Gateway and AWS Lambda integration of the RPMrepo REST API.
    This expects the python-sources of the gateway to be provided as zip
    file in the dedicated S3 bucket. The sources are taken from:
    
        https://github.com/osbuild/rpmrepo
    
    The API-Gateway provides a simple {proxy+} hook and forwards all
    requests to the AWS Lambda. This way, we basically shortcut most of the
    API Gateway features and just implement all handlers ourselves (which
    also follows most recommendations you find on the web).
    
    Signed-off-by: David Rheinsberg <david.rheinsberg@gmail.com>

--- rpmrepo.tf
+++ rpmrepo.tf
@@ -10,6 +10,9 @@
 #   * The VPC Endpoint Interface for S3 is used to provide access to RH-private
 #     data in the S3 bucket.
 #
+#   * An AWS API Gateway proxy integration with AWS Lambda backend which
+#     implements the REST API of RPMrepo.
+#
 
 ##############################################################################
 ## S3 Storage
@@ -62,4 +65,89 @@ data "aws_iam_policy_document" "rpmrepo_s3" {
 resource "aws_s3_bucket_policy" "rpmrepo_s3" {
   bucket = aws_s3_bucket.rpmrepo_s3.id
   policy = data.aws_iam_policy_document.rpmrepo_s3.json
+}
+
+##############################################################################
+## API Gateway
+
+data "aws_iam_policy_document" "rpmrepo_gateway_lambda" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "rpmrepo_gateway_lambda" {
+  assume_role_policy = data.aws_iam_policy_document.rpmrepo_gateway_lambda.json
+  name               = "rpmrepo-gateway-lambda"
+  tags = merge(
+    var.imagebuilder_tags,
+    { Name = "RPMrepo Gateway Lambda Role" },
+  )
+}
+
+resource "aws_lambda_function" "rpmrepo_gateway" {
+  function_name = "rpmrepo-gateway"
+  handler       = "lambda_function.lambda_handler"
+  role          = aws_iam_role.rpmrepo_gateway_lambda.arn
+  runtime       = "python3.8"
+  s3_bucket     = aws_s3_bucket.rpmrepo_s3.id
+  s3_key        = "code/rpmrepo-gateway/rpmrepo-gateway-main.zip"
+  tags = merge(
+    var.imagebuilder_tags,
+    { Name = "RPMrepo Gateway" },
+  )
+}
+
+resource "aws_api_gateway_rest_api" "rpmrepo_gateway" {
+  name        = "rpmrepo-gateway"
+  description = "RPMrepo Web Gateway"
+  tags = merge(
+    var.imagebuilder_tags,
+    { Name = "RPMrepo Gateway" },
+  )
+}
+
+resource "aws_api_gateway_resource" "rpmrepo_gateway_proxy" {
+  rest_api_id = aws_api_gateway_rest_api.rpmrepo_gateway.id
+  parent_id   = aws_api_gateway_rest_api.rpmrepo_gateway.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "rpmrepo_gateway_proxy" {
+  authorization = "NONE"
+  http_method   = "ANY"
+  resource_id   = aws_api_gateway_resource.rpmrepo_gateway_proxy.id
+  rest_api_id   = aws_api_gateway_rest_api.rpmrepo_gateway.id
+}
+
+resource "aws_api_gateway_integration" "rpmrepo_gateway_proxy" {
+  http_method             = aws_api_gateway_method.rpmrepo_gateway_proxy.http_method
+  integration_http_method = "POST"
+  resource_id             = aws_api_gateway_method.rpmrepo_gateway_proxy.resource_id
+  rest_api_id             = aws_api_gateway_rest_api.rpmrepo_gateway.id
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.rpmrepo_gateway.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "rpmrepo_gateway" {
+  depends_on = [
+    aws_api_gateway_integration.rpmrepo_gateway_proxy,
+  ]
+  rest_api_id = aws_api_gateway_rest_api.rpmrepo_gateway.id
+  stage_name  = "v1"
+}
+
+resource "aws_lambda_permission" "rpmrepo_gateway" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.rpmrepo_gateway.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.rpmrepo_gateway.execution_arn}/*/*"
+  statement_id  = "AllowAPIGatewayInvoke"
 }
