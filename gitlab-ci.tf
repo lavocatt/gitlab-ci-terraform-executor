9163862a3b182b7b7d9d063856a3899997aa3e62
Ondřej Budai <ondrej@budai.cz>
2021-03-31 14:52:38 +0200

    gitlab: add role that can manage spot instances
    The new role should be able to manage spot instances for our new experimental
    CI.

---
+++ gitlab-ci.tf
@@ -0,0 +1,54 @@
+data "aws_iam_policy_document" "gitlab_ci_ec2_principal" {
+  statement {
+    sid = "AllowEC2AssumeRole"
+
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "gitlab_ci" {
+  name = "gitlab_ci-${local.workspace_name}"
+
+  assume_role_policy = data.aws_iam_policy_document.gitlab_ci_ec2_principal.json
+
+  tags = merge(
+    var.imagebuilder_tags, { Name = "Image Builder GitLab CI role" },
+  )
+}
+
+resource "aws_iam_instance_profile" "gitlab_ci" {
+  name = "gitlab_ci-${local.workspace_name}"
+  role = aws_iam_role.gitlab_ci.name
+}
+
+data "aws_iam_policy_document" "gitlab_ci_manage_instances" {
+  statement {
+    sid = "gitlab_ci_manage_instances"
+
+    actions = [
+      "ec2:DescribeSpotInstanceRequests",
+      "ec2:CancelSpotInstanceRequests",
+      "ec2:RequestSpotInstances",
+      "ec2:RunInstances",
+      "ec2:TerminateInstances",
+      "ec2:DescribeInstances",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "gitlab_ci_manage_instances" {
+  name   = "gitlab_ci_manage_instances-${local.workspace_name}"
+  policy = data.aws_iam_policy_document.gitlab_ci_manage_instances.json
+}
+
+resource "aws_iam_role_policy_attachment" "gitlab_ci_manage_instances" {
+  role       = aws_iam_role.gitlab_ci.name
+  policy_arn = aws_iam_policy.gitlab_ci_manage_instances.arn
+}
