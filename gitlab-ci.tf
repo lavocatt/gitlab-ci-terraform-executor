e76e039b8406d5f444df8b719f3386d505deaf0d
Ondřej Budai <ondrej@budai.cz>
2021-04-01 11:30:39 +0200

    gitlab: fix egress traffic
    Both security groups are meant to have all egress traffix allowed.

--- gitlab-ci.tf
+++ gitlab-ci.tf
@@ -67,9 +67,10 @@ resource "aws_security_group" "gitlab_ci_runner_internal" {
   vpc_id      = data.aws_vpc.internal_vpc.id
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -95,9 +96,10 @@ resource "aws_security_group" "gitlab_ci_runner_external" {
   vpc_id      = data.aws_vpc.external_vpc.id
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
