66b3286740a71a987013607c82ebfdc662e628de
Ondřej Budai <ondrej@budai.cz>
2021-10-27 12:27:37 +0200

    workers/aoc: use port 443

--- cloud-init/partials/worker_service.sh
+++ cloud-init/partials/worker_service.sh
@@ -5,7 +5,7 @@ source /tmp/cloud_init_vars
 echo "Setting up worker services."
 
 # Prepare osbuild-composer's remote worker services and sockets.
-systemctl enable --now osbuild-remote-worker@${COMPOSER_HOST}:8700
+systemctl enable --now osbuild-remote-worker@${COMPOSER_HOST}:${COMPOSER_PORT}
 
 # Now that everything is configured, ensure monit is monitoring everything.
 systemctl enable --now monit
