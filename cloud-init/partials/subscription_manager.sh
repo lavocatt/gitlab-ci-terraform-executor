879e1482e8540df29e53e3fc67d331934003d926
Tom Gundersen <teg@jklm.no>
2021-10-28 18:11:16 +0100

    workers: add TODO
    This is mostly to force all workers to be restarted, but
    also it would make things simpler to follow if we would
    split secrets from logic.

--- cloud-init/partials/subscription_manager.sh
+++ cloud-init/partials/subscription_manager.sh
@@ -5,6 +5,7 @@ source /tmp/cloud_init_vars
 echo "Subscribing instance to RHN."
 
 # Register the instance with RHN.
+# TODO: don't store the command in a secret, only the key/org-id
 /usr/local/bin/aws secretsmanager get-secret-value \
   --endpoint-url ${SECRETS_MANAGER_ENDPOINT_URL} \
   --secret-id ${SUBSCRIPTION_MANAGER_COMMAND_ARN} | jq -r ".SecretString" > /tmp/subscription_manager_command.json
