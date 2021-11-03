25bc05452c25e3fc2bbf7318bcf7cc0591abee38
Ondřej Budai <ondrej@budai.cz>
2021-11-03 09:43:45 +0100

    workers/aoc: disable ssh
    cloudwatch now works, let's disable our access

--- workers-fleet-aoc.tf
+++ workers-fleet-aoc.tf
@@ -77,7 +77,6 @@ resource "aws_launch_template" "worker_aoc_x86" {
   name          = "imagebuilder_worker_aoc_x86"
   image_id      = data.aws_ami.rhel8_x86_prebuilt.id
   instance_type = "t3.medium"
-  key_name      = "obudai"
 
   # Allow the instance to assume the external_worker IAM role.
   iam_instance_profile {
