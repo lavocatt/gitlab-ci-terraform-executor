6d51811d83b5d237abc6e643b75b0359370112c4
Ondřej Budai <ondrej@budai.cz>
2021-11-28 20:55:44 +0100

    cloud-init: prune unused scripts
    These were all used by internal and external deployments that were removed
    by previous commits.

--- cloud-init/partials/worker_hosts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-source /tmp/cloud_init_vars
-
-echo "Deploy /etc/hosts for workers."
-
-# Set up /etc/hosts
-# TODO(mhayden): We need to convert this to DNS later when we launch.
-cat <<< "${COMPOSER_ADDRESS} ${COMPOSER_HOST}" >> /etc/hosts
