629174cc5757fc7f42a2fe6e3a9fdf4dd3fb4798
Ondřej Budai <ondrej@budai.cz>
2021-11-28 13:47:51 +0100

    workers: use more instance types
    c5ad is just c5d but with AMD processors.
    
    c6i is a new generation of intel-based instances, they are about 10% faster
    for the same price.

--- variables.tf
+++ variables.tf
@@ -19,9 +19,13 @@ variable "worker_instance_types" {
     "c5.large",
     "c5d.large",
     "c5a.large",
+    "c5ad.large",
     "c5.xlarge",
     "c5d.xlarge",
-    "c5a.xlarge"
+    "c5a.xlarge",
+    "c5ad.xlarge",
+    "c6i.large",
+    "c6i.xlarge"
   ]
 }
 
