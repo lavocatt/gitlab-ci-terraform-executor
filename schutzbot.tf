3e5c07e44c712215845a104202958c2b210df84c
Ondřej Budai <ondrej@budai.cz>
2021-12-14 14:00:20 +0100

    Revert "schutzbot: put all gitlab stuff into a module"
    This reverts commit d07cfdfd4c5f84f47c05203251032ea0f84307ff.

--- schutzbot.tf
+++
@@ -1,6 +0,0 @@
-module "gitlab-runner" {
-  source          = "./schutzbot"
-  workspace_name  = local.workspace_name
-  internal_vpc_id = data.aws_vpc.internal_vpc.id
-  external_vpc_id = data.aws_vpc.external_vpc.id
-}
