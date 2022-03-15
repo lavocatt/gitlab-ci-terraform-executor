4287cd8fc23b0b028ad5a2f8091388f4cec740c4
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2022-03-15 13:41:21 +0100

    Revert "locals: Scale down aoc prod"
    This reverts commit 796ea55754e7c901b45858653bc9197a79b80e7d.

--- locals.tf
+++ locals.tf
@@ -2,5 +2,5 @@ locals {
   # Set the workspace_name without the imagebuilder- prefix.
   workspace_name = var.TFC_WORKSPACE_NAME != "" ? trimprefix(var.TFC_WORKSPACE_NAME, "imagebuilder-") : terraform.workspace
 
-  spot_fleet_worker_aoc_count = 0
+  spot_fleet_worker_aoc_count = local.workspace_name == "staging" ? 0 : 16
 }
